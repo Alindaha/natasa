3b07217df4aed06ff47e788bf2cd4275fa3d85d9
Alindaha <alindaharmanny@gmail.com>
2019-10-30 18:18:58 +0000

    Opdracht2

--- JS/H1/H1O2.js
+++ JS/H1/H1O2.js
@@ -1,12 +1,23 @@
 function setup() {
   var myCanvas = createCanvas(450,450);
-  background('orange');
+  //background('orange');
+  background('darkred');
   myCanvas.parent('processing');
   noLoop();
 }
 
 function draw() {
+  /*
   noStroke();
   fill('darkred');
   triangle(225,0,0,450,450,450);
+  */
+  //opdracht 11
+  noStroke();
+  fill('pink');
+  triangle(0,225,450,450,450,0);
+  //opdracht 12
+  noStroke();
+  fill('purple');
+  triangle(0,0,0,450,450,225);
 }
