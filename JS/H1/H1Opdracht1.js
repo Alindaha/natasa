912192dd518cf10c4368e48bbe8043f67a314f4d
Alindaha <alindaharmanny@gmail.com>
2019-10-30 17:20:41 +0000

    Opdracht1

--- JS/H1/H1Opdracht1.js
+++ JS/H1/H1Opdracht1.js
@@ -1,21 +1,26 @@
 function setup() {
-  var myCanvas = createCanvas(1000,500);
-  background('orange');
+  var myCanvas = createCanvas(450,450);
+  background('grey');
   myCanvas.parent('processing');
   noLoop();
 }
 
 function draw() {
   // groene cirkel zonder rand
-  
+
   noStroke();
   fill('green');
-  ellipse(300,200,400);
-  
+  ellipse(225,225,400);
+
+  // witte circel zonder rand
+  noStroke();
+  fill('white');
+  ellipse(225,225,300);
+
   // witte rechthoek met rode rand
-  
-  stroke('red');
+
+  stroke('green');
   fill('white');
   strokeWeight(10);
-  rect(650,100,250,300);  
+  rect(125,125,200,200);
 }
